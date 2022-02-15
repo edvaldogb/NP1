aa4b050110e3ab6d0506917204dd4c80f5a1fb49
edvaldogb <edvaldogoncalvesbarbosa@gmail.com>
2022-02-15 00:38:13 -0300

    Instalar Boostrap

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
-app.get("/", (req,res)=>{
+
+app.get("/", (req,res) => {
     res.render('index');
 });
 
-app.listen(8080,()=>{
-    console.log('Servidor Iniciado com Sucesso.')
+app.get("/perguntar", (req,res) => {
+    res.render('perguntar');
+});
+
+app.listen(8080,() => {
+    console.log('Servidor Iniciado com Sucesso.');
 });
