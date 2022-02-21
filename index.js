e65da01df731c1c5b741e6925fcd0d0fa1338b52
edvaldogb <edvaldogoncalvesbarbosa@gmail.com>
2022-02-20 22:22:35 -0300

    Trabalhando com formularios.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // Plataforma de Perguntas e Respostas.
+
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+// BodyParser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Rotas.
 app.get("/", (req,res) => {
     res.render('index');
 });
 
 app.get("/perguntar", (req,res) => {
     res.render('perguntar');
+});
+
+app.post("/salvarpergunta", (req,res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;    
+    res.send("Formulário recebido. Tíltulo "+ titulo +"  "+ "descrição" + "  " + descricao);
 });
 
 app.listen(8080,() => {
